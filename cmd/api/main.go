62e00cd88037ceaec1da4d110adaefd876e8d177
Boonyarit Rousamran <hok@Boonyarits-MacBook-Air.local>
2023-09-22 14:25:26 -0700

    add graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/hokkung/go-groceries/di"
 )
@@ -12,5 +17,18 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	container.Server.Start()
+	go func() {
+		container.Server.Start()
+	}()
+
+	gracefulShutdown := make(chan os.Signal, 1)
+	signal.Notify(gracefulShutdown, syscall.SIGTERM, syscall.SIGINT)
+	<-gracefulShutdown
+
+	container.Server.Stop()
+
+	_, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+	cancle()
+	
+	fmt.Println("application gracefully shutdown.")
 }
